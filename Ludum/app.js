a97bfad6bffe2863beb4248e362cef8aea7fbc85
Martin Jonsson <martinjonsson01@gmail.com>
2019-03-04 19:23:17 +0100

    Added Barba.js for SPA transitions.

--- Ludum/app.js
+++ Ludum/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,8 +11,13 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultLayout: 'main',
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/'
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,12 +29,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- Ludum/routes/index.js
+++ Ludum/routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express'});
+});
+
+/* GET page 1. */
+router.get('/page1', function(req, res, next) {
+  res.render('page1', { title: 'Express'});
+});
+
+/* GET page 2. */
+router.get('/page2', function(req, res, next) {
+  res.render('page2', { title: 'Express'});
 });
 
 module.exports = router;

9033fe8acd4970a3ffa75dd05131ad4dc755683e
Martin Jonsson <martinjonsson01@gmail.com>
2019-07-15 21:42:06 +0200

    Fixes google calendar integration

--- client/src/components/schedule/SchedulePage.jsx
+++ client/src/components/schedule/SchedulePage.jsx
@@ -1,31 +1,43 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Card from "@material/react-card";
 import { Grid, Row, Cell } from "@material/react-layout-grid";
 import withTitle from "../common/withTitle.jsx";
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import { Calendar } from '@fullcalendar/core';
-import googleCalendarPlugin from '@fullcalendar/google-calendar';
-import '@fullcalendar/core/main.css';
-import '@fullcalendar/daygrid/main.css';
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { Calendar } from "@fullcalendar/core";
+import googleCalendarPlugin from "@fullcalendar/google-calendar";
+import "@fullcalendar/core/main.css";
+import "@fullcalendar/daygrid/main.css";
 
 function SchedulePage() {
-  var calendarEl = document.getElementById('calendar');
-  let calendar = new Calendar(calendarEl, {
-    plugins: [googleCalendarPlugin,dayGridPlugin, timeGridPlugin, interactionPlugin],
-    googleCalendarApiKey: 'AIzaSyC06FeHq3RykmYYInvo8U_nezBwAyOpzlc',
-    events: {
-      googleCalendarId: 'https://calendar.google.com/calendar/b/1?cid=Z2EubGJzLnNlX2R0bDl0ZTdvMjlkYWxqNzRkb2kyYW5sMWtrQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20'
-    }
-  });
+  const calendarRef = useRef(null);
+  useEffect(() => {
+    new Calendar(calendarRef.current, {
+      plugins: [googleCalendarPlugin, dayGridPlugin, timeGridPlugin, interactionPlugin],
+      googleCalendarApiKey: "AIzaSyC06FeHq3RykmYYInvo8U_nezBwAyOpzlc",
+      events: {
+        googleCalendarId: "ga.lbs.se_dtl9te7o29dalj74doi2anl1kk@group.calendar.google.com"
+      }
+    });
+  }, [calendarRef]);
+
   return (
     <Grid>
       <Row>
         <Cell columns={12}>
           <Card className="full-height">
-           <FullCalendar defaultView="dayGridMonth" plugins={[ googleCalendarPlugin,dayGridPlugin, timeGridPlugin, interactionPlugin ]} />;
+            <FullCalendar
+              ref={calendarRef}
+              id="calendar"
+              defaultView="dayGridMonth"
+              plugins={[googleCalendarPlugin, dayGridPlugin, timeGridPlugin, interactionPlugin]}
+              googleCalendarApiKey="AIzaSyC06FeHq3RykmYYInvo8U_nezBwAyOpzlc"
+              events={{
+                googleCalendarId: "ga.lbs.se_dtl9te7o29dalj74doi2anl1kk@group.calendar.google.com"
+              }}
+            />
           </Card>
         </Cell>
       </Row>
